b2731328b6322325a6f2a235f38a1bf9f2e54579
Pabin234 <codemode365@gmail.com>
2024-05-29 17:09:29 +0545

    partialized token parms

--- src/utils/token.utils.ts
+++ src/utils/token.utils.ts
@@ -1,10 +1,18 @@
 import jwt from 'jsonwebtoken';
 import { vars } from '@/config';
 import { ApiError } from '@/utils';
+import type { AccountType } from '@/config/enums.config';
 
 const secret = vars.jwt.jwt_secret;
 
-const generateToken = (payload: { userId: string }, expiresIn: string) => {
+const generateToken = (
+  payload: Partial<{
+    userId: string;
+    roles: AccountType[];
+    permissions: unknown[];
+  }>,
+  expiresIn: string
+) => {
   return jwt.sign(payload, secret, { expiresIn: expiresIn });
 };
 
