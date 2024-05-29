524fcd9b877e71bfd749f84f9116bb5157bbc0f8
Pabin234 <codemode365@gmail.com>
2024-05-29 15:03:35 +0545

    server setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-import { db } from "@/db/db";
+import '@/config';
+import { morganLogger, logger } from '@/config';
+import ApiRoutes from '@/routes';
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from 'express';
+import { ApiError } from '@/utils';
+import { Handler } from '@/helpers';
 
-console.log("Hello world!");
+const PORT = process.env.PORT || 4300;
+const app = express();
 
-db;
+app.use(express.json());
+app.use(morganLogger.dbReqHandler);
+app.use(morganLogger.consoleReqHandler);
+
+Handler.routeWrapper(ApiRoutes);
+app.use('/api/v1', ApiRoutes);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+  res.status(500).send(`Something broke! \n ${err}!`);
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'server is up!' });
+});
+
+app.listen(PORT, () => {
+  logger.info('hello world');
+  logger.info(`App listening on: http://localhost:${PORT}`);
+});
