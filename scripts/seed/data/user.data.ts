d38bddf2d0aef8d412e7a5ef4f4ed0bf9cc663ba
Pabin234 <codemode365@gmail.com>
2024-05-31 15:14:38 +0545

    dummy user data

--- scripts/seed/data/user.data.ts
+++ scripts/seed/data/user.data.ts
@@ -1,15 +1,23 @@
 import mongoose from 'mongoose';
 import { HashUtils } from '@/utils';
 import { AccountStatusType } from '@/config/enums.config';
-import type { UserType } from '@/types/model';
+import type { UserSchemaType } from '@/types/model';
 
-const userData: ({ _id: mongoose.Types.ObjectId } | UserType)[] = [
+const userData: ({ _id: mongoose.Types.ObjectId } | UserSchemaType)[] = [
   {
     _id: new mongoose.Types.ObjectId('605f6c9a9a13e72a015b0883'),
     username: 'PabinTester',
     email: 'pabintester@gmail.com',
     password: HashUtils.hashPassword('123456'),
-    roles: ['605f6c9a9a13e72a015b0884'],
+    roles: [new mongoose.Types.ObjectId('605f6c9a9a13e72a015b0884')],
+    status: AccountStatusType.ACTIVE,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('605f6c9a9a13e72a015b0884'),
+    username: 'Pabin',
+    email: 'pabin@gmail.com',
+    password: HashUtils.hashPassword('123456'),
+    roles: [new mongoose.Types.ObjectId('605f6c9a9a13e72a015b0885')],
     status: AccountStatusType.ACTIVE,
   },
 ];
