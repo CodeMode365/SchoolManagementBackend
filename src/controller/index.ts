ee35bdba1f328d0cdf5e8baca6a4ddb1676fca75
Pabin234 <codemode365@gmail.com>
2024-05-31 15:23:19 +0545

    extended controller provider

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,1 +1,3 @@
 export { default as AuthController } from './auth.controller';
+export { default as StudentController } from './student.controller';
+export { default as ParentController } from './parent.controller';
