cd1419cae49f44ac13f7640905b3cea0dae195af
Pabin234 <codemode365@gmail.com>
2024-05-31 15:21:55 +0545

    comment unused

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,31 +1,10 @@
-import {
-  LogIn,
-  LogOut,
-  Register,
-  changePassword,
-  createNewRole,
-  deleteAccount,
-  deleteRole,
-  deleteSession,
-  forgetPassword,
-  getAllSession,
-  getRoles,
-  myInfo,
-  refreshToken,
-  resetPassword,
-  revokeToken,
-  setup2Fa,
-  updateMyInfo,
-  updateRole,
-  verify2Fa,
-} from "@/controller/auth.route";
+import { AuthController } from "@/controller";
 import { Router } from "express";
 
-const route = Router();
+const router = Router();
 
 //registration
-route.post("/register", Register);
-
+router.post("/register", AuthController.Register);
 /*
 route.post("/register/teacher", registerTeacher);
 route.post("/register/student", registerStudent);
@@ -35,35 +14,38 @@ route.post("/register/admin", registerAdmin);
 */
 
 //login
-route.post("/login", LogIn);
-route.post("/logout", LogOut);
+router.post("/login", AuthController.LogIn);
 
 //password management
-route.post("/forget-password", forgetPassword);
-route.post("/reset-password", resetPassword);
-route.post("/change-password", changePassword);
+router.post("/forget-password", AuthController.forgetPassword);
+router.post("/reset-password", AuthController.resetPassword);
+router.post("/change-password", AuthController.changePassword);
 
 //user profile handling
-route.get("/me", myInfo);
-route.put("/me", updateMyInfo);
-route.delete("/me", deleteAccount);
+router.get("/me", AuthController.myInfo);
+/*
+route.post('/logout', AuthController.LogOut);
+
+route.put('/me', AuthController.updateMyInfo);
+route.delete('/me', AuthController.deleteAccount);
 
 //session management
-route.get("/sessions", getAllSession);
-route.delete("/sessions/:sessionId", deleteSession);
+route.get('/sessions', AuthController.getAllSession);
+route.delete('/sessions/:sessionId', AuthController.deleteSession);
 
 //Role management
-route.get("/roles", getRoles);
-route.post("/roles", createNewRole);
-route.put("/roles/:roleId", updateRole);
-route.delete("/roles/:roleId", deleteRole);
+route.get('/roles', AuthController.getRoles);
+route.post('/roles', AuthController.createNewRole);
+route.put('/roles/:roleId', AuthController.updateRole);
+route.delete('/roles/:roleId', AuthController.deleteRole);
 
 //Token management
-route.post("/token/refresh", refreshToken);
-route.post("revoke", revokeToken);
+route.post('/token/refresh', AuthController.refreshToken);
+route.post('revoke', AuthController.revokeToken);
 
 //Two-Factor Authentication
-route.get("/2fa/setup", setup2Fa);
-route.get("/2fa/verify", verify2Fa);
+route.get('/2fa/setup', AuthController.setup2Fa);
+route.get('/2fa/verify', AuthController.verify2Fa);
+*/
 
-export default route;
+export default router;
