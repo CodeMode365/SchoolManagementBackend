32500ec29d2a07d6f524128424c4fa4396f92984
Pabin234 <codemode365@gmail.com>
2024-05-31 15:16:53 +0545

    extended auth specific methods

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,14 @@
 import { omit } from 'lodash';
 
-import type { RoleType } from '@/types/model';
+import type {
+  AdminSchemaType,
+  ParentSchemaType,
+  RoleSchemaType,
+  StaffSchemaType,
+  StudentSchemaType,
+  TeacherSchemaType,
+  UserSchemaType,
+} from '@/types/model';
 import { vars } from '@/config';
 import { Token, User } from '@/models';
 import { RoleService } from '.';
@@ -15,7 +23,7 @@ USER_STATUS_EXCEPTIONS[AccountStatusType.INACTIVE] = 'Account is deactivated.';
 USER_STATUS_EXCEPTIONS[AccountStatusType.PENDING] = 'Account is not verified.';
 USER_STATUS_EXCEPTIONS[AccountStatusType.DELETED] = 'Account has been deleted.';
 
-const getRolesAndPermissions = (data: RoleType[]) => {
+const getRolesAndPermissions = (data: RoleSchemaType[]) => {
   let ps: unknown[] = [];
   const rs = data.map((d) => {
     ps.push(d.permissions);
@@ -35,7 +43,7 @@ const login = async (payload: {
     .where({ $or: [{ username }, { email }] })
     .populate({
       path: 'roles',
-      populate: [{ path: 'permissions', model: 'Permission' }],
+      // populate: [{ path: 'permissions', model: 'Permission' }],
     });
   if (!dbUser) throw ApiError.notFound('Email not found.');
   if (dbUser.status !== AccountStatusType.ACTIVE)
@@ -65,7 +73,7 @@ const register = async (payload: {
   email: string;
   password: string;
   username: string;
-  role: RoleType;
+  role: RoleSchemaType;
 }) => {
   const { email } = payload;
   const user = await User.findOne().where({ email });
@@ -96,7 +104,96 @@ const register = async (payload: {
   };
 };
 
+const forgetPassword = async (payload: { email: string }) => {
+  const { email } = payload;
+  const user = await User.findOne().where({ email });
+  if (!user) throw ApiError.notFound('Email not found');
+  const tokenPayload = { userId: user._id as string };
+  const verificationToken = TokenUtils.generateToken(
+    tokenPayload,
+    vars.jwt.verify_expiry
+  );
+  const resetLink = `${vars.client_url}/reset-password?token=${verificationToken}&user=${user.email}`;
+  // sendEmail(user.email, EMAIL_TYPES.RESET_PASSWORD, { resetLink });
+};
+
+const resetPassword = async (payload: {
+  email: string;
+  token: string;
+  newPassword: string;
+}) => {
+  const { email, token } = payload;
+  const user = await User.findOne().where({ email });
+  if (!user) throw ApiError.notFound('Email not found');
+  const dbToken = await Token.findOne().where({
+    $and: [token, { user: user._id }],
+  });
+  if (!dbToken) throw ApiError.notAuthorized('Token not found');
+  const encPass = HashUtils.hashPassword(payload.newPassword);
+  await User.findByIdAndUpdate(user._id, { password: encPass });
+};
+
+const changePassword = async (payload: {
+  userId: string;
+  oldPassword: string;
+  newPassword: string;
+}) => {
+  const { userId, oldPassword, newPassword } = payload;
+  const user = await User.findById(userId);
+  if (!user) throw ApiError.notFound('User not found');
+  const passCompare = HashUtils.comparePassword(oldPassword, user.password);
+  if (!passCompare) throw ApiError.notAuthorized('Invalid password.');
+  const encPass = HashUtils.hashPassword(newPassword);
+  await User.findByIdAndUpdate(userId, { password: encPass });
+};
+
+const myInfo = async (payload: { userId: string }) => {
+  const { userId } = payload;
+  const dbUser = await User.findById(userId);
+  if (!dbUser) throw ApiError.notFound('User not found');
+  const { rs, ps } = getRolesAndPermissions(dbUser.roles);
+  return {
+    user: {
+      ...omit(dbUser.toJSON(), ['password', 'roles']),
+      roles: rs,
+      permissions: ps,
+    },
+  };
+};
+
+const updateMyInfo = async (
+  userInfo: {
+    userId: string;
+    userRoles: RoleSchemaType[];
+  },
+  payload: {
+    roleSpecific: Partial<
+      | TeacherSchemaType
+      | ParentSchemaType
+      | StudentSchemaType
+      | StaffSchemaType
+      | AdminSchemaType
+    >;
+    userSpecific: Partial<UserSchemaType>;
+  }
+) => {
+  const dbUser = await User.findById(userInfo.userId);
+  if (!dbUser) throw ApiError.notFound('User not Found!');
+  console.log(payload);
+};
+
+const isUserActive = async (userId: string) => {
+  const dbUser = await User.findById(userId);
+  return dbUser?.status === AccountStatusType.ACTIVE;
+};
+
 export default {
   register,
   login,
+  forgetPassword,
+  resetPassword,
+  changePassword,
+  myInfo,
+  isUserActive,
+  updateMyInfo,
 };
