9f99cfdb53f0813fc7cd6ffd0fbbbb9bb8f40cbe
Pabin234 <codemode365@gmail.com>
2024-05-31 15:21:40 +0545

    service provider

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+export { default as AuthService } from "./auth.service";
+export { default as RoleService } from "./role.service";
+export { default as StudentService } from "./student.service";
+export { default as TeacherService } from "./teacher.service";
+export { default as ParentService } from "./parent.service";
