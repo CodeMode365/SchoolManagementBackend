03051aea0fec8b33addb15027ef11f4e5b426266
Pabin234 <codemode365@gmail.com>
2024-05-31 15:19:27 +0545

    async route handlers

---
+++ src/helpers/handler.ts
@@ -0,0 +1,31 @@
+import { logger } from '@/config';
+import type { Request, Response, NextFunction, Router } from 'express';
+
+type AsyncHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any>;
+
+const asyncHandler =
+  (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((e: any) => {
+      logger.error(e.message);
+      console.log(e);
+      next(e);
+    });
+  };
+
+const routeWrapper = (router: Router) => {
+  router.stack.forEach((layer: any) => {
+    if (layer.route) {
+      layer.route.stack.forEach((layer: any) => {
+        layer.handle = asyncHandler(layer.handle);
+      });
+    } else if (layer.name == 'router') {
+      routeWrapper(layer.handle);
+    }
+  });
+};
+
+export default { asyncHandler, routeWrapper };
