f7ff630a5df60332de06e6f6b1097b18a001ae43
Pabin234 <codemode365@gmail.com>
2024-05-31 15:14:56 +0545

    error route management

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,20 @@ app.use(morganLogger.consoleReqHandler);
 
 Handler.routeWrapper(ApiRoutes);
 app.use('/api/v1', ApiRoutes);
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+app.use((err: any, req: Request, res: Response) => {
   if (err instanceof ApiError) {
     return res.status(err.statusCode).json({ error: err.message });
   }
+  logger.error(err);
   res.status(500).send(`Something broke! \n ${err}!`);
 });
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'server is up!' });
+});
+
+app.all('*', (req, res) => {
+  return res.status(404).json({ error: 'Route not found!' });
 });
 
 app.listen(PORT, () => {
