4baa9439b999ffc6e3e50aa0a73e3822bdf235a6
Pabin234 <codemode365@gmail.com>
2024-06-04 13:12:55 +0545

    added redis client

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ export { default as logger } from './logger.config';
 export { default as dbConnection } from './db.config';
 export { default as vars } from './vars.config';
 export { default as morganLogger } from './morgan.config';
+export { default as redis } from './redis.config';
 export * as EnumVar from './enums.config';
