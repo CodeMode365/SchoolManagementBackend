02d7472030e7a51090b544e3bc92c4bc2163b712
Pabin234 <codemode365@gmail.com>
2024-06-06 21:23:33 +0545

    role controller with crud operations

--- src/controller/role.controller.ts
+++ src/controller/role.controller.ts
@@ -1,3 +1,4 @@
+import { ValChecker } from '@/helpers';
 import { Role } from '@/models';
 import { CrudService } from '@/services';
 import type { RoleSchemaType } from '@/types/model';
@@ -5,19 +6,53 @@ import type { Request, Response } from 'express';
 
 const CrudSrv = new CrudService<RoleSchemaType>(Role);
 
-const getAllRoles = async (req: Request, res: Response) => {
+const getAll = async (req: Request, res: Response) => {
   const roles = await CrudSrv.getAll({}, { page: 1, limit: 10 });
   return res.json(roles);
 };
-const getRoleByName = async (req: Request, res: Response) => {};
-const updateRole = async (req: Request, res: Response) => {};
-const removeRole = async (req: Request, res: Response) => {};
-const createRole = async (req: Request, res: Response) => {};
+
+const update = async (req: Request, res: Response) => {
+  const { roleId } = ValChecker.checkMissingFields(['roleId'], req.params);
+  const {
+    name,
+    permissions = [],
+    description = '',
+  } = ValChecker.checkMissingFields(
+    ['name', 'permissions', 'description'],
+    req.body
+  );
+  const role = await CrudSrv.update(roleId, { name, permissions, description });
+  return res.json(role);
+};
+
+const remove = async (req: Request, res: Response) => {
+  const { roleId } = ValChecker.checkMissingFields(['roleId'], req.params);
+  await CrudSrv.remove(roleId);
+  return res.json({ message: 'Role deleted!' });
+};
+
+const create = async (req: Request, res: Response) => {
+  const {
+    name,
+    permissions = [],
+    description = '',
+  } = ValChecker.checkMissingFields(
+    ['name', 'permissions', 'description'],
+    req.body
+  );
+  const role = await CrudSrv.create({ name, permissions, description });
+  return res.json(role);
+};
+
+const getById = async (req: Request, res: Response) => {
+  const role = await CrudSrv.getOne(req.params.roleId);
+  return res.json(role);
+};
 
 export default {
-  getAllRoles,
-  getRoleByName,
-  createRole,
-  updateRole,
-  removeRole,
+  getAll,
+  create,
+  update,
+  remove,
+  getById,
 };
