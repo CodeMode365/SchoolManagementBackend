aedfbc8b19a50300963cc85ef31663e791ba24ce
Pabin234 <codemode365@gmail.com>
2024-06-06 21:38:47 +0545

    api log handler

--- src/controller/apiLog.controller.ts
+++ src/controller/apiLog.controller.ts
@@ -1,3 +1,4 @@
+import { ValChecker } from '@/helpers';
 import { ApiLog } from '@/models';
 import { CrudService } from '@/services';
 import type { ApiLogSchemaType } from '@/types/model';
@@ -13,4 +14,26 @@ const getAll = async (req: Request, res: Response) => {
   return res.json(apiLogs);
 };
 
-export default { getAll };
+const getById = async (req: Request, res: Response) => {
+  const apiLog = await CrudSrv.getOne(req.params.apiLogId);
+  return res.json(apiLog);
+};
+
+const create = async (req: Request, res: Response) => {
+  const apiLog = await CrudSrv.create(req.body);
+  return res.json(apiLog);
+};
+
+const remove = async (req: Request, res: Response) => {
+  const { apiLogId } = ValChecker.checkMissingFields(['apiLogId'], req.params);
+  await CrudSrv.remove(apiLogId);
+  return res.json({ message: 'ApiLog deleted!' });
+};
+
+const update = async (req: Request, res: Response) => {
+  const { apiLogId } = ValChecker.checkMissingFields(['apiLogId'], req.params);
+  const apiLog = await CrudSrv.update(apiLogId, req.body);
+  return res.json(apiLog);
+};
+
+export default { getAll, getById, update, remove, create };
--- src/routes/apiLog.route.ts
+++ src/routes/apiLog.route.ts
@@ -1,9 +1,38 @@
-import { Router } from 'express';
+import { vars } from '@/config';
 import { ApiLogController } from '@/controller';
+import { cacheMiddleware, can } from '@/middleware';
+import { Router } from 'express';
+import { PERMISSIONS } from '@/config/constants.config';
 
 const router = Router();
+const cacheKey = vars.cacheKey.logs; // Define the appropriate cache key for apiLogs
 
-router.get('/all', ApiLogController.getAll);
-// router.get('/by-org/:orgId');
+router.get(
+  '/all',
+  can(PERMISSIONS.LOGS.READ),
+  cacheMiddleware.getCache(cacheKey),
+  ApiLogController.getAll
+);
+
+router.post(
+  '/add',
+  can(PERMISSIONS.LOGS.WRITE),
+  cacheMiddleware.clearCache(cacheKey),
+  ApiLogController.create
+);
+
+router.patch(
+  '/update/:logId',
+  can(PERMISSIONS.LOGS.UPDATE),
+  cacheMiddleware.clearCache(cacheKey),
+  ApiLogController.update
+);
+
+router.delete(
+  '/remove/:logId',
+  can(PERMISSIONS.LOGS.DELETE),
+  cacheMiddleware.clearCache(cacheKey),
+  ApiLogController.remove
+);
 
 export default router;
