0b90182d57b6e18222f97f9a11378a3e7a1a4d1e
Pabin234 <codemode365@gmail.com>
2024-06-13 20:18:07 +0545

    extended routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,9 @@ import { default as ParentRoute } from './parent.route';
 import { default as AttendanceRoute } from './attendance.route';
 import { default as ApiLogRoute } from './apiLog.route';
 import { default as RoleRoute } from './role.route';
+import { default as EventRoute } from './event.route';
+import { default as BillRoute } from './bill.route';
+import { default as ClassRoute } from './class.route';
 import { Router } from 'express';
 
 const router = Router();
@@ -14,5 +17,8 @@ router.use('/parents', ParentRoute);
 router.use('/attendances', AttendanceRoute);
 router.use('/apiLogs', ApiLogRoute);
 router.use('/roles', RoleRoute);
+router.use('/events', EventRoute);
+router.use('/bills', BillRoute);
+router.use('/classes', ClassRoute);
 
 export default router;
