df7943e567bd0371a90d4b67eba47744dff96c02
Pabin234 <codemode365@gmail.com>
2024-06-18 13:03:37 +0545

    token service extended

--- src/utils/token.utils.ts
+++ src/utils/token.utils.ts
@@ -2,21 +2,31 @@ import jwt, { type JwtPayload } from 'jsonwebtoken';
 import { vars } from '@/config';
 import { ApiError } from '@/utils';
 
-const secret = vars.jwt.jwt_secret;
+const { jwt_secret, login_expiry, refresh_expiry, verify_expiry } = vars.jwt;
 
-const generateToken = (payload: JwtPayload, expiresIn: string) => {
-  return jwt.sign(payload, secret, { expiresIn: expiresIn });
+const generateAuthToken = (payload: JwtPayload) => {
+  return jwt.sign(payload, jwt_secret, { expiresIn: login_expiry });
+};
+
+const generateMailingToken = (payload: JwtPayload) => {
+  return jwt.sign(payload, jwt_secret, { expiresIn: verify_expiry });
+};
+
+const generateRefreshToken = (payload: JwtPayload) => {
+  return jwt.sign(payload, jwt_secret, { expiresIn: refresh_expiry });
 };
 
 const validateToken = (token: string) => {
   try {
-    return { data: jwt.verify(token, secret) };
+    return { data: jwt.verify(token, jwt_secret) };
   } catch {
     throw ApiError.badRequest('Token expired.');
   }
 };
 
 export default {
-  generateToken,
+  generateAuthToken,
+  generateMailingToken,
+  generateRefreshToken,
   validateToken,
 };
